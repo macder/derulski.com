d9ef13f0dce8870aca5d00b1f5046a4dd43d0f88
Maciej Derulski <macder7@gmail.com>
2017-12-21 17:23:12 -0500

    init js manifest - placeholder

---
+++ ui/src/scripts/main.js
@@ -0,0 +1,13 @@
+/*import Header from '../patterns/_patterns/02-organisms/00-global/00-header/header';
+
+const components = [{
+  name: 'header',
+  Component: Header,
+}];
+
+components.forEach((c) => {
+  Array.from(document.querySelectorAll(`.c-${c.name}`)).forEach((el) => {
+    const element = el;
+    element.component = new c.Component(el);
+  });
+});*/

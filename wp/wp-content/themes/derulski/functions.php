5067ef3ef2b857d5a095b623ed18cdb75bc8c2f9
Maciej Derulski <macder7@gmail.com>
2017-12-30 04:17:40 -0500

    use jquery 3

--- wp/wp-content/themes/derulski/functions.php
+++ wp/wp-content/themes/derulski/functions.php
@@ -58,7 +58,7 @@ class DerulskiSite extends TimberSite {
    */
   public function include_jquery() {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js', array(), null, true);
+    wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, true);
     wp_enqueue_script("jquery");
   }
 

b69b35e67c193134034bb19d06778c0d88be9a09
Maciej Derulski <macder7@gmail.com>
2017-12-31 00:52:07 -0500

    let wp generate title tag - instead of this messy non-sense...

--- wp/wp-content/themes/derulski/functions.php
+++ wp/wp-content/themes/derulski/functions.php
@@ -13,6 +13,7 @@ class DerulskiSite extends TimberSite {
     add_theme_support( 'post-formats' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'menus' );
+    add_theme_support( 'title-tag' );
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
     // add stuff to Timber context

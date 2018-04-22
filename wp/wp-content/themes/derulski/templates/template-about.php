b72961db6d5abb920613ffd41049c6e9efd04711
Maciej Derulski <macder7@gmail.com>
2018-04-21 20:10:58 -0400

    WP - about page base

---
+++ wp/wp-content/themes/derulski/templates/template-about.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Template Name: About
+ */
+
+/**
+ * The Template for About page
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$context['post'] = new TimberPost();
+
+$context['hero'] = array(
+  'image' => ( new TimberImage( get_field('hero_image') ) )->src(),
+  'text' => get_field('hero_heading'),
+  'sub_text' => get_field('hero_sub_text'),
+);
+
+
+Timber::render( array( 'about.twig' ), $context );
